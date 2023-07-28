b954b17d7fdc3b7f81b5c32d36c0bc4e941170aa
MihailoBojic <mihailo.bojic@digitalcontrol.me>
2023-07-28 08:43:07 +0200

    End of secction 8

--- entities/AppUser.cs
+++ entities/AppUser.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Threading.Tasks;
+using API.extensions;
+using Microsoft.AspNetCore.Identity;
 
 namespace API.entities
 {
@@ -11,5 +15,23 @@ namespace API.entities
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }    
+        public DateOnly DateOfBirth { get; set; }   
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public DateTime LastActive { get; set; } = DateTime.UtcNow;
+        public string Gender { get; set; }
+        public string Introduction { get; set; }    
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }   
+
+        public string City { get; set; }
+        public string Country { get; set; } 
+        public List<Photo> Photos { get; set; } = new();
+
+        // public int GetAge() 
+        // {
+        //     return DateOfBirth.CalcuateAge();
+        // }
+
     }
 }
---
+++ helpers/AutoMappersProfiles.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.entities;
+using API.extensions;
+using AutoMapper;
+
+namespace API.helpers
+{
+    public class AutoMappersProfiles : Profile
+    {
+        public AutoMappersProfiles() 
+        
+        {
+            CreateMap<AppUser, MemberDto>()
+            .ForMember(dest => dest.PhotoUrl,
+             opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url))
+             .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalcuateAge()));
+            CreateMap<Photo, PhotoDto>();            
+        }
+    }
+}
