07f027e6aff8dbe7e55dfaffb97cab13b901984b
MihailoBojic <mihailo.bojic@digitalcontrol.me>
2023-08-08 09:50:46 +0200

    End of section 10

--- helpers/AutoMappersProfiles.cs
+++ helpers/AutoMappersProfiles.cs
@@ -18,7 +18,8 @@ namespace API.helpers
             .ForMember(dest => dest.PhotoUrl,
              opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url))
              .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalcuateAge()));
-            CreateMap<Photo, PhotoDto>();            
+            CreateMap<Photo, PhotoDto>();       
+            CreateMap<MemberUpdateDto, AppUser>();     
         }
     }
 }
